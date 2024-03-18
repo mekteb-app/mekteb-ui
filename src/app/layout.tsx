1036710caa822d53043bfa8fc717de0b19fe8ead
Mirza Curic <mirzacuric@Mirzas-MacBook-Pro-2.local>
2024-03-18 13:54:13 +0100

    Added favicon logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
 "use client";
-import "jsvectormap/dist/css/jsvectormap.css";
 import "flatpickr/dist/flatpickr.min.css";
 import "@/css/satoshi.css";
 import "@/css/style.css";
 import React, { useEffect, useState } from "react";
 import Loader from "@/components/common/Loader";
+import Head from "next/head";
 
 export default function RootLayout({
   children,
@@ -22,6 +22,34 @@ export default function RootLayout({
 
   return (
     <html lang="en">
+      <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="../assets/logo/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="../assets/logo/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="../assets/logo/favicon-16x16.png"
+        />
+        <link rel="manifest" href="../assets/logo/site.webmanifest" />
+        <link
+          rel="mask-icon"
+          href="../assets/logo/safari-pinned-tab.svg"
+          color="#5bbad5"
+        />
+        <link rel="icon" href="./favicon.ico" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
       <body suppressHydrationWarning={true}>
         <div className="dark:bg-boxdark-2 dark:text-bodydark">
           {loading ? <Loader /> : children}
