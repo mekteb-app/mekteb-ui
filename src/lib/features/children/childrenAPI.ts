d00f374320cd51e8dd3d631d26be14f192a566d8
Mirza Curic <mirzacuric@192.168.1.11>
2024-04-25 07:51:16 +0200

    Create child and added children options to the app state

--- src/lib/features/children/childrenAPI.ts
+++ src/lib/features/children/childrenAPI.ts
@@ -1,13 +1,27 @@
-import { baseApiCall } from "@/lib/baseApiCall";
+import { baseApiCall, baseApiCallBody } from "@/lib/baseApiCall";
 import { IPagination } from "@/interfaces/IPagination";
 import CustomError from "@/types/custom-error";
 import { IChild } from "@/interfaces/IChild";
+import { IChildPayload } from "@/interfaces/IChildPayload";
+import { IChildOption } from "@/interfaces/IChildOption";
 
 interface IChildrenResponse {
   data: IChild[];
   message: string;
   status: number;
   count: number;
+}
+
+interface IChildrenOptionsResponse {
+  data: IChildOption[];
+  message: string;
+  status: number;
+}
+
+interface IChildResponse {
+  data: IChild;
+  message: string;
+  status: number;
 }
 
 // A mock function to mimic making an async request for data
@@ -17,6 +31,38 @@ export const fetchChildren = async ({ page, count }: IPagination) => {
     "GET"
   );
   const result: IChildrenResponse = await response.json();
+
+  if (response.status !== 200) {
+    const customError = new CustomError(result.message, response.status);
+    throw new Error(customError.stringify());
+  }
+
+  return result;
+};
+
+export const createChild = async (data: IChildPayload) => {
+  const response = await baseApiCallBody(
+    `${process.env.NEXT_PUBLIC_API_URL}/children`,
+    "POST",
+    {},
+    data
+  );
+  const result: IChildResponse = await response.json();
+
+  if (response.status !== 201) {
+    const customError = new CustomError(result.message, response.status);
+    throw new Error(customError.stringify());
+  }
+
+  return result;
+};
+
+export const fetchChildrenOptions = async () => {
+  const response = await baseApiCall(
+    `${process.env.NEXT_PUBLIC_API_URL}/children/options`,
+    "GET"
+  );
+  const result: IChildrenOptionsResponse = await response.json();
 
   if (response.status !== 200) {
     const customError = new CustomError(result.message, response.status);
--- src/lib/features/children/childrenSlice.ts
+++ src/lib/features/children/childrenSlice.ts
@@ -1,16 +1,26 @@
 import { createAppSlice } from "@/lib/createAppSlice";
 import { IChild } from "@/interfaces/IChild";
-import { fetchChildren } from "./childrenAPI";
+import {
+  createChild,
+  fetchChildren,
+  fetchChildrenOptions,
+} from "./childrenAPI";
+import { IChildPayload } from "@/interfaces/IChildPayload";
+import { toast } from "react-toastify";
+import { IError } from "@/interfaces/IError";
+import { IChildOption } from "@/interfaces/IChildOption";
 
 export interface ChildrenSliceState {
   children: IChild[];
-  status: "idle" | "loading" | "failed";
+  childrenOptions: IChildOption[];
+  status: "idle" | "loading" | "failed" | "created";
   count: number;
   error: any;
 }
 
 const initialState: ChildrenSliceState = {
   children: [],
+  childrenOptions: [],
   status: "idle",
   count: 0,
   error: undefined,
@@ -47,14 +57,69 @@ export const childrenSlice = createAppSlice({
         },
       }
     ),
+    createChildAsync: create.asyncThunk(
+      async (data: IChildPayload) => {
+        const response = await createChild(data);
+        // The value we return becomes the `fulfilled` action payload
+        return response;
+      },
+      {
+        pending: (state) => {
+          state.status = "loading";
+        },
+        fulfilled: (state, action) => {
+          state.status = "created";
+          state.children = [action.payload.data, ...(state.children || [])];
+          state.count = state.count || 0 + 1;
+          toast.success("Child created successfully");
+        },
+        rejected: (state, action) => {
+          console.log("rejected");
+          state.status = "failed";
+          state.error = action.error as IError;
+        },
+      }
+    ),
+
+    setChildrenOptionsAsync: create.asyncThunk(
+      async () => {
+        const response = await fetchChildrenOptions();
+        // The value we return becomes the `fulfilled` action payload
+        return response;
+      },
+      {
+        pending: (state) => {
+          state.status = "loading";
+          state.error = undefined;
+        },
+        fulfilled: (state, action) => {
+          state.status = "idle";
+          state.error = undefined;
+          state.childrenOptions = action.payload.data || [];
+        },
+        rejected: (state, action) => {
+          state.status = "failed";
+          state.error = action.error;
+        },
+      }
+    ),
 
     resetError: create.reducer((state) => {
+      state.error = undefined;
+    }),
+
+    resetChildrenState: create.reducer((state) => {
+      state.children = [];
+      state.childrenOptions = [];
+      state.status = "idle";
+      state.count = 0;
       state.error = undefined;
     }),
   }),
   // Selectors
   selectors: {
     selectChildren: (Children) => Children.children || [],
+    selectChildrenOptions: (Children) => Children.childrenOptions || [],
     selectChildrenCount: (Children) => Children.count || 0,
     selectChildrenStatus: (Children) => Children.status || "idle",
     selectChildrenError: (Children) => Children.error || undefined,
@@ -62,11 +127,18 @@ export const childrenSlice = createAppSlice({
 });
 
 // Action creators are generated for each case reducer function.
-export const { setChildrenAsync, resetError } = childrenSlice.actions;
+export const {
+  setChildrenAsync,
+  setChildrenOptionsAsync,
+  createChildAsync,
+  resetError,
+  resetChildrenState,
+} = childrenSlice.actions;
 
 // Selectors returned by `slice.selectors` take the root state as their first argument.
 export const {
   selectChildren,
+  selectChildrenOptions,
   selectChildrenCount,
   selectChildrenStatus,
   selectChildrenError,
