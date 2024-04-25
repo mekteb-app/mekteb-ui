476f6da956c49109c9b5c33403e2e9268886dbcd
Mirza Curic <mirzacuric@192.168.1.11>
2024-04-25 07:48:36 +0200

    Initial route for the sidebar dashboard

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -114,10 +114,9 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
               {/* <!-- Menu Item Dashboard --> */}
               <li>
                 <Link
-                  href="/dashboard"
+                  href="/"
                   className={`group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark dark:hover:bg-meta-4 ${
-                    pathname.includes("calendar") &&
-                    "bg-graydark dark:bg-meta-4"
+                    pathname === "/" && "bg-graydark dark:bg-meta-4"
                   }`}
                 >
                   <svg
